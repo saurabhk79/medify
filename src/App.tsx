8ca144c2a0dc0625908e74bd68822d6d92c3d05a
Saurabh <imsauby@gmail.com>
2024-05-17 14:46:10 +0530

    added base styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
-import React from 'react'
+import React from "react";
+import "./App.css"
 
-const App:React.FC = () => {
+const App: React.FC = () => {
   return (
     <div>
+      <div className="top-headline">
+        The health and well-being of our patients and their health care team
+        will aways be our priority, so we follow the best practices for
+        cleanliness
+      </div>
       <div>Hello</div>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
